cff1a65e11ecac42bccbb1781979e0f662f179ea
Joe Pettersson <joe8bit@gmail.com>
2017-08-08 09:15:12 +0100

    Fix bug in src/to-intermediate-obj.js where wrong parameter was used

--- src/to-intermediate-obj.js
+++ src/to-intermediate-obj.js
@@ -3,7 +3,7 @@
 const _ = require('lodash');
 
 module.exports = (rawObj) => {
-    return _.extend.apply(_, raw.root.children.map((child) => {
+    return _.extend.apply(_, rawObj.root.children.map((child) => {
         let obj = {};
         obj[child.name] = child.content
         return obj;
