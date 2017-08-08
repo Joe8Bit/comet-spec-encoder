8ca12fb3c50018e390e7274caf01b0664d303ed6
Joe Pettersson <joe8bit@gmail.com>
2017-08-08 09:14:06 +0100

    Fix bug in index.js where wrong file was required

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fromXmlString = require('./src/from-xml-string');
 const toIntermediateObj = require('./src/to-intermediate-obj');
 const coerceTypes = require('./src/coerce-types');
-const validate = require('./src/validate');
+const validate = require('./src/validate-obj');
 const toXmlString = require('./src/to-xml-string');
 
 const deserialize = (str) => {
